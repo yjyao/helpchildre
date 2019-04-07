967a80ced284388f8b54cb54589ee93cd9c3b791
Jy <yjyao1112@gmail.com>
2019-04-06 20:33:11 -0700

    Add a conv layer, acc 67.6 loss 0.15.

--- cnn.py
+++ cnn.py
@@ -84,14 +84,28 @@ class CNN(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=18,
+                out_channels=48,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
         self.fc1 = nn.Sequential(
-          nn.Linear(18 * (((32 + 2*1 - 3)//1+1)//2)**2, 64),
+          nn.Linear(48 * (((16 + 2*1 - 3)//1+1)//2)**2, 512),
           nn.ReLU(),
         )
-        self.fc2 = torch.nn.Linear(64, 10)
+        self.fc2 = torch.nn.Linear(512, 10)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.conv2(x)
         x = x.view(x.size(0), -1)
         x = self.fc1(x)
         x = self.fc2(x)
